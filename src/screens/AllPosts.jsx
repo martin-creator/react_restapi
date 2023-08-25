1464636fb7539b1c5879d35483c66e38d921b047
martin-creator <martinlubowa@outlook.com>
2023-08-26 00:08:32 +0300

    Updated all posts page

--- src/screens/AllPosts.jsx
+++ src/screens/AllPosts.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
 
 function AllPosts() {
   return (
-    <div>AllPosts</div>
-  )
+    <div>
+      <section className=" container pt-2">
+        <h3 className="text-center text-uppercase py-4">All Posts</h3>
+        <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-12">
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="d-grid">
+                  <a href="#" className="btn btn-warning">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-12">
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="d-grid">
+                  <a href="#" className="btn btn-warning">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-12">
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1575517111478-7f6afd0973db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <div className="d-grid">
+                  <a href="#" className="btn btn-warning">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default AllPosts
+export default AllPosts;
