5c372e0e2d96de01256031ec49694a3cabad9476
martin-creator <martinlubowa@outlook.com>
2023-08-27 10:37:51 +0300

    Successfully implemented fetch user data

--- src/screens/PostDetail.jsx
+++ src/screens/PostDetail.jsx
@@ -9,13 +9,24 @@ function PostDetail() {
     const [user, setUser] = useState([]); // store all posts
     const {postId, userId } = useParams() // get params from url using object destructuring
 
-    const getPost = ()=>{
+    const getPostAndUser = ()=>{
         console.log("Post Id is", postId)
         console.log(`${API_BASE_URL}/posts/${postId}`)
         axios.get(`${API_BASE_URL}/posts/${postId}`)
         .then((response) => {
             console.log(response.data)
             setPost(response.data)
+            axios.get(`${API_BASE_URL}/users/${userId}`)
+            .then((response) => {
+                console.log(response.data)
+                setUser(response.data)
+            }
+            )
+            .catch((error) => {
+                console.log(error)
+            
+            }
+            )
         })
         .catch((error) => {
             console.log(error)
@@ -27,7 +38,7 @@ function PostDetail() {
 
 // we want to load data on page load of this component
     useEffect(() => {
-        getPost()
+        getPostAndUser()
     }, [])
 
 
