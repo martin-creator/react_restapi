cb5179dd3e4c56bb89676998f49039cbfc4b69a2
martin-creator <martinlubowa@outlook.com>
2023-08-27 09:15:13 +0300

    Used axios to fetch data

--- src/screens/PostDetail.jsx
+++ src/screens/PostDetail.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { API_BASE_URL } from '../config/constant'
 
 function PostDetail() {
+    const [post, setPost] = useState([]); // store all posts
+    const [user, setUser] = useState([]); // store all posts
     const {postId, userId } = useParams() // get params from url using object destructuring
+
+    const getPost()=>{
+        axios.get(`${API_BASE_URL}/posts/${postId}`)
+        .then((response) => {
+            console.log(response.data)
+            setPost(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        
+        }
+        )
+    }
+
+
+// we want to load data on page load of this component
+    useEffect(() => {
+        getPost()
+    }, [])
+
+
   return (
     <div>PostDetail Post-{postId}, User-{userId} </div>
   )
