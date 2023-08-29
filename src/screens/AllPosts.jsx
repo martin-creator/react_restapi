67e8b98626724aed06ef9cf111cf88e188c8c21b
martin-creator <martinlubowa@outlook.com>
2023-08-29 10:05:19 +0300

    Added CRUD buttons

--- src/screens/AllPosts.jsx
+++ src/screens/AllPosts.jsx
@@ -13,11 +13,10 @@ function AllPosts() {
     fetch(`${API_BASE_URL}/posts`)
       .then((response) => response.json())
       .then((json) => {
-        console.log(json)
+        console.log(json);
         setPosts(json);
         setLoader(false);
       });
-
   };
 
   // we want to load data on page load of this component
@@ -32,39 +31,54 @@ function AllPosts() {
       <section className=" container pt-2">
         <h3 className="text-center text-uppercase py-4">All Posts</h3>
         <div className="row">
-          { 
-
-          loader ? 
-          <div className="col-12 text-center">
-          <div className="spinner-border" role="status">
-          <span className="sr-only">Loading...</span>
-          </div>
-          </div>
-           :
-          
-          posts.map((post, index,) => {
-
-          return (<div key={index} className="col-lg-4 col-md-4 col-sm-12">
-            <div className="card">
-              <img
-                src="https://source.unsplash.com/random/400*400/?city,night"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">{post.title}</h5>
-                <p className="card-text">
-                 {post.body}
-                </p>
-                <div className="d-grid">
-                  <Link to={`/posts/${post.id}/${post.userId}`} className="btn btn-warning">
-                    Read More
-                  </Link>
-                </div>
+          {loader ? (
+            <div className="col-12 text-center">
+              <div className="spinner-border" role="status">
+                <span className="sr-only">Loading...</span>
               </div>
             </div>
-          </div>)
-          })}
+          ) : (
+            posts.map((post, index) => {
+              return (
+                <div key={index} className="col-lg-4 col-md-4 col-sm-12">
+                  <div className="card">
+                    <img
+                      src="https://source.unsplash.com/random/400*400/?city,night"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{post.title}</h5>
+                      <p className="card-text">{post.body}</p>
+                      <div className="d-flex justify-content between">
+                        <Link
+                          to={`/posts/${post.id}/${post.userId}`}
+                          className="btn btn-primary text-uppercase"
+                        >
+                          <i className="fa-solid fa-location-arrow me-1"></i>
+                          Details
+                        </Link>
+                        <Link
+                          to={`/create/${post.id}/${post.userId}`}
+                          className="btn btn-warning text-uppercase"
+                        >
+                          <i className="fa-solid fa-pen-to-square me-1"></i>Edit
+                        </Link>
+                        <button
+                          onClick={() => {
+                            deletePost(post.id);
+                          }}
+                          className="btn btn-danger text-uppercase"
+                        >
+                          <i className="fa-solid fa-trash me-1"></i>Delete
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          )}
         </div>
       </section>
     </div>
