a237651c68833c49f056c014e6b579f368ea70e5
martin-creator <martinlubowa@outlook.com>
2023-09-01 02:04:18 +0300

    removed duplicate import

--- src/screens/PostDetail.jsx
+++ src/screens/PostDetail.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 import { API_BASE_URL } from '../config/constant'
 import './PostDetail.css'
